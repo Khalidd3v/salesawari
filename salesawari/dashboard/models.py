226f4581e69e50c007e280e310bf692d635c41d5
khalidd3v <khalidbinalikhan@gmail.com>
2024-10-01 15:23:57 +0500

    made model for saving customer support messages

--- salesawari/dashboard/models.py
+++ salesawari/dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import Account
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class ContactUs(TimeStampedModel):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True, null=True)
+    full_name = models.CharField(max_length=32)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=32, null=True, blank=True)
+    message = models.TextField()
+
+
+    def __str__(self):
+        return self.full_name
