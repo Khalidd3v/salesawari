8eca95c82fc85eea4a55eb604ac2c5bd28e693b8
khalidd3v <khalidbinalikhan@gmail.com>
2024-10-28 00:18:20 +0500

    filter made for cars

--- salesawari/apis/urls.py
+++ salesawari/apis/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
      path('favorite-vehicle/', FavouriteVehicleToggleView.as_view()),
      path('send_message/', SendMessageView.as_view(), name='send_message'),
      path('fetch_messages/', FetchMessagesView.as_view(), name='fetch_messages'),
+     path('filter-vehicles/', VehicleFilterView.as_view(), name='fetch_messages'),
 ]
--- salesawari/apis/views.py
+++ salesawari/apis/views.py
@@ -8,6 +8,8 @@ from rest_framework.authentication import TokenAuthentication
 from chat.models import *
 from django.shortcuts import get_object_or_404, render
 from .serializers import *
+from seller.models import Vehicle
+from django.db.models import Q
 
 class FavouriteVehicleToggleView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -69,3 +71,52 @@ class FetchMessagesView(APIView):
         messages = Message.objects.filter(conversation_id=conversation_id)
         serializer = MessageSerializer(messages, many=True)
         return Response(serializer.data)
+    
+
+class VehicleFilterView(APIView):
+    permission_classes = []
+    
+    def post(self, request):
+        # Get filter data
+        colors = request.data.get('colors', [])
+        engine_range = request.data.get('engine_range', [])
+        price_range = request.data.get('price_range', {})
+        
+        # Start with all unsold vehicles
+        queryset = Vehicle.objects.filter(is_sold=False)
+        
+        # Apply color filter if selected
+        if colors:
+            queryset = queryset.filter(color__in=colors)
+        
+        # Apply engine range filter if selected
+        if engine_range:
+            engine_queries = Q()
+            for range_str in engine_range:
+                min_val, max_val = map(int, range_str.split('-'))
+                engine_queries |= Q(engine__gte=min_val) & Q(engine__lte=max_val)
+            queryset = queryset.filter(engine_queries)
+        
+        # Apply price range filter
+        if price_range:
+            max_price = price_range.get('max')
+            if max_price:
+                queryset = queryset.filter(price__lte=max_price)
+        
+        # Prepare response data
+        vehicles_data = []
+        for vehicle in queryset:
+            vehicle_data = {
+                'id': vehicle.id,
+                'name': vehicle.name,
+                'seats': vehicle.seats,
+                'doors': vehicle.doors,
+                'fuel_type': vehicle.fuel_type,
+                'engine': vehicle.engine,
+                'vehicle_type': vehicle.vehicle_type,
+                'price': vehicle.price,
+                'image': vehicle.images.first().image.url if vehicle.images.exists() else None
+            }
+            vehicles_data.append(vehicle_data)
+        
+        return Response(vehicles_data)
